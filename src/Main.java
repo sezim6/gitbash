84c2b7d0c691ffce262825a79509f5004f0e04b1
User <sezimsatybaldieva22@gmail.com>
2023-10-26 15:05:11 +0300

    usd

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,4 @@ public class Main {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         System.out.println(Arrays.stream(array).filter(a->a%2==0).max(Integer::compareTo).orElse(0));
     }
-    }//https://github.com/sezim6/gitbash.git
+    }
